5b082acb8892871911fc25393e5e9f77b67b48ce
Gyuho Lee <gyuho.lee@avalabs.org>
2022-10-23 21:06:13 -0700

    serde_format: update
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- src/serde_format/big_int_hex.rs
+++ src/serde_format/big_int_hex.rs
@@ -5,7 +5,6 @@ pub fn serialize<S>(bi: &BigInt, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
 {
-    // ref. https://docs.rs/chrono/0.4.19/chrono/struct.DateTime.html#method.to_rfc3339_opts
     serializer.serialize_str(&crate::big_int_to_lower_hex(bi))
 }
 
@@ -15,6 +14,5 @@ where
 {
     let s = String::deserialize(deserializer)?;
 
-    // ref. https://docs.rs/chrono/0.4.19/chrono/struct.DateTime.html#method.parse_from_rfc3339
     crate::from_hex_to_big_int(&s).map_err(serde::de::Error::custom)
 }
